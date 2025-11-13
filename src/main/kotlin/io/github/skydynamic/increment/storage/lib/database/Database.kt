5e9da2219e198e4d8fe9c287bd3667f0ca807708
SkyDynamic <tcplxw@outlook.com>
2025-11-13 09:28:04 +0800

    .

--- src/main/kotlin/io/github/skydynamic/increment/storage/lib/database/Database.kt
+++ src/main/kotlin/io/github/skydynamic/increment/storage/lib/database/Database.kt
@@ -4,9 +4,8 @@ import com.google.gson.Gson
 import io.github.skydynamic.increment.storage.lib.manager.IDatabaseManager
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
+import org.jetbrains.exposed.sql.transactions.transaction
 
 private val gson = Gson()
 
